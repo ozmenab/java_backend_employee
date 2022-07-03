9cfa7c436bcf0eb5cf6f4a6664d9d6984bc7530d
Abdullah <abdullah.ozmen.06@gmail.com>
2022-07-03 13:37:50 +0300

    Repository(Jpa Hibernate)

---
+++ src/main/java/com/abdullahozmen/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.abdullahozmen.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+}
